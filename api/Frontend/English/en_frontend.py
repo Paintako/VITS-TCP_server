b081812716d68507d1e4af8e486f58a99a1bf3de
paintakotako <p76111652@gs.ncku.edu.tw>
2024-01-03 00:08:25 +0800

    feat: English frontend implemented

---
+++ api/Frontend/English/en_frontend.py
@@ -0,0 +1,43 @@
+from typing import Any, List
+import os
+import sys
+
+sys.path.append(f'{os.path.dirname(os.path.abspath(__file__))}/../../')
+from logs import service_logger
+
+dict_path = os.path.dirname(os.path.abspath(__file__))
+pinyin = {}
+with open(os.path.join(dict_path,'pinyin.dict'), 'r', encoding='utf8') as f:
+    lines = f.readlines()
+
+for line in lines:
+    line = line.strip().split("\t")
+    pinyin[line[0]] = line[1]
+
+class en_frontend():
+    def __init__(self) -> None:
+        self.punc = "：，；。？！“”‘’':,;.?!"
+        self.pinyin = pinyin
+        self.logger = service_logger.ServiceLogger()
+        
+    def get_phonemes(self, sentence: str) -> List[List[str]]:
+        phonemes = []    
+        self.logger.info(f'Processing sentence: {sentence}')
+
+        for word in sentence.split(" "):
+            word = word.upper().strip()
+            try:
+                ph = self.pinyin[word]
+                for p in ph.split(" "):    
+                    phonemes.append(p.strip())
+            except: 
+                self.logger.error(f'Word {word} not found in dictionary')
+                continue
+        print(phonemes)
+        return phonemes
+    
+if __name__ == "__main__":
+    frontend = en_frontend()
+    sentence = 'Trying to be better'
+    phonemes = frontend.get_phonemes(sentence)
+    print(phonemes)
