961087f3737e571a730ce912a5b89fc4c2d82440
paintakotako <p76111652@gs.ncku.edu.tw>
2024-02-06 01:29:06 +0800

    [FIX] Modifying logger format

--- api/logs/service_logger.py
+++ api/logs/service_logger.py
@@ -1,38 +1,37 @@
 import logging
-import logging.handlers
+import logging.config
 
-class ServiceLogger:
-    def __init__(self):
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - (%(filename)s:%(lineno)d) - %(message)s ')
+def service_logger():
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'normal': {  # the name of formatter
+                'format': "[*] Server %(asctime)s %(levelname)s %(ipaddr)s:%(message)s",
+                'datefmt':'%Y-%m-%d %H:%M:%S'
+            },
+        },
+        'handlers': {
+            'console': {  # the name of handler
+                'class': 'logging.StreamHandler',  # emit to sys.stderr(default)
+                'formatter': 'normal', 
+            }, # use the above "normal" formatter
+            'file': {  # the name of handler
+                'class': 'logging.handlers.RotatingFileHandler',  # emit to disk file
+                'filename': 'server.log',  # the path of the log file
+                'formatter': 'normal',  # use the above "normal" formatter
+                'maxBytes': 1024*1024,
+                'backupCount': 3
+            },
+        },
+        'loggers': {
+            'logger': {  # the name of logger
+                'handlers': ['console', 'file'],  # use the above "console1" and "console2" handler
+                'level': 'INFO',  # logging level
+            },
+        },
+    }
+    logging.config.dictConfig(config=LOGGING)
+    logger = logging.getLogger('logger')
 
-        # Create a file handler for the main logger
-        file_handler = logging.handlers.RotatingFileHandler('logs/service.log', maxBytes=1024*1024, backupCount=3)
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
-
-        # Create a file handler for the 'file' logger
-        file_logger_handler = logging.handlers.RotatingFileHandler('logs/file.log', maxBytes=1024*1024, backupCount=3)
-        file_logger_handler.setFormatter(formatter)
-
-        # Create a logger for the 'file' handler
-        file_logger = logging.getLogger('file')
-        file_logger.setLevel(logging.DEBUG)
-        file_logger.addHandler(file_logger_handler)
-
-    # info: Confirmation that things are working as expected.
-    def info(self, message):
-        self.logger.info(message)
-
-    # debug: for detailed information, typically of interest only when diagnosing problems.
-    def debug(self, message):
-        self.logger.debug(message)
-
-    # error: Due to a more serious problem, the software has not been able to perform some function.
-    def error(self, message):
-        self.logger.error(message)
-
-    # warning: An indication that something unexpected happened, or indicative of some problem in the near future.
-    def warning(self, message):
-        self.logger.warning(message)
+    return logger
